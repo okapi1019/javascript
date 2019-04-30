9d98152484b30670ac53e97198faaea009bd8a77
岡田真樹 <coffeelatter@gmail.com>
2019-04-30 18:59:24 +0900

    テキストメッセージの追加。

--- ajax.js
+++ ajax.js
@@ -2,13 +2,15 @@ const endpoint = "https://script.google.com/macros/s/AKfycbwxBi696KAlZwspbsp4rs_
 
 $('#submit-button').on('click',function(){
     var selected = $('#select').val();
+    var text = $('#chat').val();
 
     $.ajax({
         type: 'get',
         url: endpoint,
         dataType: 'jsonp',
         data:{
-            param: selected
+            param: selected,
+            text: text
         },
         success: out => {
             alert(out.answer);
